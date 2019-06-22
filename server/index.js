c5da66ce9c200582117b9c7801897a82c46bdfec
Sherin Binu <sherinbinu@hotmail.com>
2019-06-22 14:41:33 +0530

    add helmet and snyk

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const helmet = require('helmet');
 const config = require('./config');
 
 const app = express();
+
+app.use(helmet());
 
 app.listen(config.port, error => {
   console.log(`Listening on port ${config.port}`);
