3ff483791f905adf4d7897b4f3e8aefde1cf39b2
Sherin Binu <sherinbinu@hotmail.com>
2019-06-27 19:29:17 +0530

    setup rate limiting

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const config = require('./config');
 const userRoutes = require('./routes/user.route');
@@ -6,6 +7,13 @@ const authRoutes = require('./routes/auth.route');
 
 const app = express();
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use(limiter);
 app.use(express.json({ limit: 1e6 }));
 app.use(helmet());
 
