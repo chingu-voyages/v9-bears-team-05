36fc02e99365d54974e598736d49dc858cece735
Sherin Binu <sherinbinu@hotmail.com>
2019-07-04 00:56:25 +0530

    add crud operations for styles and clothes

--- psql/migration.sql
+++ psql/migration.sql
@@ -1,4 +1,5 @@
-CREATE TABLE user_ (
+CREATE TABLE user_
+(
   user_id SERIAL PRIMARY KEY,
   first_name VARCHAR,
   last_name VARCHAR,
@@ -7,45 +8,51 @@ CREATE TABLE user_ (
   hashed_password VARCHAR
 );
 
-CREATE TABLE closet (
+CREATE TABLE closet
+(
   closet_id SERIAL PRIMARY KEY,
   closet_name VARCHAR,
   user_id INTEGER NOT NULL REFERENCES user_(user_id) ON DELETE CASCADE,
   parent_closet INTEGER DEFAULT NULL REFERENCES closet(closet_id)
 );
 
-CREATE TABLE collection_ (
+CREATE TABLE collection_
+(
   collection_id SERIAL PRIMARY KEY,
   collection_name VARCHAR,
   user_id INTEGER NOT NULL REFERENCES user_(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE style (
+CREATE TABLE style
+(
   style_id SERIAL PRIMARY KEY,
   style_name VARCHAR,
-  collection_id INTEGER NOT NULL REFERENCES collection_(collection_id) ON DELETE CASCADE,
   user_id INTEGER NOT NULL REFERENCES user_(user_id) ON DELETE CASCADE,
   contents VARCHAR DEFAULT NULL
 );
 
-CREATE TABLE cloth (
+CREATE TABLE cloth
+(
   cloth_id SERIAL PRIMARY KEY,
   cloth_name VARCHAR,
   image_url VARCHAR DEFAULT NULL,
   user_id INTEGER NOT NULL REFERENCES user_(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE cloth_closet_mapping (
+CREATE TABLE cloth_closet_mapping
+(
   cloth_id INTEGER NOT NULL REFERENCES cloth(cloth_id),
   closet_id INTEGER NOT NULL REFERENCES closet(closet_id)
 );
 
-CREATE TABLE cloth_style_mapping (
+CREATE TABLE cloth_style_mapping
+(
   cloth_id INTEGER NOT NULL REFERENCES cloth(cloth_id),
   style_id INTEGER NOT NULL REFERENCES style(style_id)
 );
 
-CREATE TABLE style_collection_mapping (
+CREATE TABLE style_collection_mapping
+(
   style_id INTEGER NOT NULL REFERENCES style(style_id),
   collection_id INTEGER NOT NULL REFERENCES collection_(collection_id)
 )
--- psql/seeds.sql
+++ psql/seeds.sql
@@ -1,37 +1,91 @@
-INSERT INTO user_ (first_name, last_name, email, hashed_password) VALUES ('Test', 'Name', 'test@gmail.com', '$2a$12$VNTUIxGZ4C5eg7fh/SrEqO.k.ja5BUKu9B63FjTpzNKUu.2ubl4Pe');
+INSERT INTO user_
+  (first_name, last_name, email, hashed_password)
+VALUES
+  ('Test', 'Name', 'test@gmail.com', '$2a$12$VNTUIxGZ4C5eg7fh/SrEqO.k.ja5BUKu9B63FjTpzNKUu.2ubl4Pe');
 /* password is pdpdpP123*/
 
-INSERT INTO user_ (first_name, last_name, email, hashed_password) VALUES ('Another', 'User', 'anotheruser@gmail.com', '$2a$12$C8YwOe/gWRvUtQXL80EEueHeWtxaSDwlD0RhJr4X1eFuHLszVLtDK');
+INSERT INTO user_
+  (first_name, last_name, email, hashed_password)
+VALUES
+  ('Another', 'User', 'anotheruser@gmail.com', '$2a$12$C8YwOe/gWRvUtQXL80EEueHeWtxaSDwlD0RhJr4X1eFuHLszVLtDK');
 /* password is pdpdpD123*/
 
-INSERT INTO closet (closet_name, user_id) VALUES ('Tops', '1');
+INSERT INTO closet
+  (closet_name, user_id)
+VALUES
+  ('Tops', '1');
 
-INSERT INTO closet (closet_name, user_id) VALUES ('Casual', '2');
+INSERT INTO closet
+  (closet_name, user_id)
+VALUES
+  ('Casual', '2');
 
-INSERT INTO closet (closet_name, user_id) VALUES ('Bottoms', '1');
+INSERT INTO closet
+  (closet_name, user_id)
+VALUES
+  ('Bottoms', '1');
 
-INSERT INTO closet (closet_name, user_id) VALUES ('Outerwear', '2');
+INSERT INTO closet
+  (closet_name, user_id)
+VALUES
+  ('Outerwear', '2');
 
-INSERT INTO closet (closet_name, user_id, parent_closet) VALUES ('Blouses', '1', '1');
+INSERT INTO closet
+  (closet_name, user_id, parent_closet)
+VALUES
+  ('Blouses', '1', '1');
 
-INSERT INTO closet (closet_name, user_id, parent_closet) VALUES ('Tank Tops', '1', '1');
+INSERT INTO closet
+  (closet_name, user_id, parent_closet)
+VALUES
+  ('Tank Tops', '1', '1');
 
-iNSERT INTO collection_ (collection_name, user_id) VALUES ('Weeekend - Summer', '1');
+iNSERT INTO collection_
+  (collection_name, user_id)
+VALUES
+  ('Weeekend - Summer', '1');
 
-INSERT INTO collection_ (collection_name, user_id) VALUES ('Weekend - Winter', '1');
+INSERT INTO collection_
+  (collection_name, user_id)
+VALUES
+  ('Weekend - Winter', '1');
 
-INSERT INTO collection_ (collection_name, user_id) VALUES ('Gym', '1');
+INSERT INTO collection_
+  (collection_name, user_id)
+VALUES
+  ('Gym', '1');
 
-INSERT INTO collection_ (collection_name, user_id) VALUES ('Vacation', '2');
+INSERT INTO collection_
+  (collection_name, user_id)
+VALUES
+  ('Vacation', '2');
 
-INSERT INTO style (style_name, collection_id, user_id) VALUES ('My Fav', '1', '1');
+INSERT INTO style
+  (style_name, user_id)
+VALUES
+  ('My Fav', '1');
 
-INSERT INTO style (style_name, collection_id, user_id) VALUES ('My Fav #2', '1', '1');
+INSERT INTO style
+  (style_name, user_id)
+VALUES
+  ('My Fav #2', '1');
 
-INSERT INTO style (style_name, collection_id, user_id) VALUES ('My Fav', '4', '2');
+INSERT INTO style
+  (style_name, user_id)
+VALUES
+  ('My Fav', '2');
 
-INSERT INTO cloth (cloth_name, user_id) VALUES ('Casual Grey Shirt', '1');
+INSERT INTO cloth
+  (cloth_name, user_id)
+VALUES
+  ('Casual Grey Shirt', '1');
 
-INSERT INTO cloth (cloth_name, user_id) VALUES ('Crop Top', '1');
+INSERT INTO cloth
+  (cloth_name, user_id)
+VALUES
+  ('Crop Top', '1');
 
-INSERT INTO cloth (cloth_name, user_id) VALUES ('Denim Jeans', '1');
+INSERT INTO cloth
+  (cloth_name, user_id)
+VALUES
+  ('Denim Jeans', '1');
