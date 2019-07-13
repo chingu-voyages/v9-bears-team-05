4fc434f178f4516b9e821aa300aa515b1314f6da
Sherin Binu <sherinbinu@hotmail.com>
2019-07-13 15:52:52 +0530

    add registration

--- client/pages/auth.vue
+++ client/pages/auth.vue
@@ -3,9 +3,22 @@
     <v-flex xs12 sm8 md6>
       <v-card>
         <v-container>
-          <v-card-title class="headline">Login</v-card-title>
+          <v-card-title v-if="isLogin" class="headline">Login</v-card-title>
+          <v-card-title v-else class="headline">Register</v-card-title>
           <v-card-text>
             <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                v-if="!isLogin"
+                v-model="firstName"
+                :rules="[rules.required]"
+                label="First Name"
+              ></v-text-field>
+              <v-text-field
+                v-if="!isLogin"
+                v-model="lastName"
+                :rules="[rules.required]"
+                label="Last Name"
+              ></v-text-field>
               <v-text-field
                 v-model="email"
                 :rules="[rules.required, rules.emailValidation]"
@@ -19,6 +32,20 @@
                 :rules="[rules.required, rules.passwordValidation]"
                 @click:append="showPassword = !showPassword"
               ></v-text-field>
+              <p v-if="isLogin">
+                New user? Click
+                <span class="link primary--text" @click="isLogin = false"
+                  >here</span
+                >
+                to register
+              </p>
+              <p v-else>
+                Already have an account? Click
+                <span class="link primary--text" @click="isLogin = true"
+                  >here</span
+                >
+                to login
+              </p>
               <v-btn :disabled="!valid" color="success" @click="validate"
                 >Submit</v-btn
               >
@@ -36,7 +63,10 @@ export default {
   layout: 'auth',
   data() {
     return {
+      isLogin: true,
       valid: true,
+      firstName: '',
+      lastName: '',
       email: '',
       password: '',
       showPassword: false,
@@ -53,18 +83,40 @@ export default {
   methods: {
     validate() {
       if (this.$refs.form.validate()) {
-        this.$axios
-          .$post(
-            '/auth/login',
-            {
-              email: this.email,
-              password: this.password
-            },
-            { withCredentials: true }
-          )
-          .then(res => this.$router.push('/'))
-          .catch(() => {})
+        if (this.isLogin) {
+          this.login()
+        } else {
+          this.register()
+        }
       }
+    },
+    login() {
+      this.$axios
+        .$post(
+          '/auth/login',
+          {
+            email: this.email,
+            password: this.password
+          },
+          { withCredentials: true }
+        )
+        .then(res => this.$router.push('/'))
+        .catch(() => {})
+    },
+    register() {
+      this.$axios
+        .$post('/users', {
+          email: this.email,
+          password: this.password,
+          firstName: this.firstName,
+          lastName: this.lastName
+        })
+        .then(res => {
+          if (!res.error) {
+            this.login()
+          }
+        })
+        .catch(() => {})
     },
     reset() {
       this.$refs.form.reset()
@@ -72,3 +124,10 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+span.link {
+  color: blue;
+  cursor: pointer;
+}
+</style>
